4afaadec1f3011895e9235440f12265bfd56fabc
Kunal Shantilal <KunalShantilal@Kunals-MacBook-Pro.local>
2017-06-14 15:43:20 +1200

    added in a new slideshow using Swiper plugin

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
 $(document).ready(function(){
-	$('#myCarousel1, #myCarousel2').carousel();
+	
+	var mySwiper = new Swiper('.swiper-container', {
+		pagination: '.swiper-pagination',
+		nextButton: '.swiper-button-next',
+		prevButton: '.swiper-button-prev',
+		slidesPerView: 1,
+		paginationClickable: true, //If you click on the dots they will go to that slde
+		spaceBetween: 30,
+		loop: true, //Loops over the slider
+		speed: 1000,
+		autoplay: 3000,
+		// autoplayDisableOnInteraction: false, //When user interacts with the slider it continues
+		// Cube Effect on Slideshow
+		effect: "cube", 
+			  cube:	{
+			  slideShadows: true,
+			  shadow: true,
+			  shadowOffset: 20,
+			  shadowScale: 0.94
+			}
+	
+
+
+	});
 
 });
 
